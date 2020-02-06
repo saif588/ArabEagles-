2ad9ea7c140e75d4ba5ef022eb76eca8851b5e6e
saif588 <52879046+saif588@users.noreply.github.com>
2020-02-06 03:28:39 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -155,7 +155,7 @@ client.on("message", async message => {
             معلومات عن السيرفر : ${prefix}server
             برودكاست للأونلاين فقط : ${prefix}bco
             يعرض لك عدد المتبندين من سيرفرك : ${prefix}banned
-            رابط سيرفر الدعم الفني : https://discord.gg/k6MvfrC
+            رابط سيرفر الدعم الفني : https://discord.gg/hsJBjVH
             **`);
             message.channel.sendEmbed(help); // رابط السيرفر يعود الى سيرفر CODES .
     }
