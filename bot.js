12b7db83535959db4ce0f21e1c9d7c340f139d00
saif588 <52879046+saif588@users.noreply.github.com>
2020-07-09 18:39:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`$bc |نسور العرب`,"http://twitch.tv/Death Shop")
+client.user.setGame(`$bc |ISU`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
